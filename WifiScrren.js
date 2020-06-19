eddb5df6906dc201f68f2d8d6489d8955a64d8f5
Shilpa Shendre <shilpashendre@winjit.com>
2020-06-19 18:23:50 +0530

    shown connected device list and available connection list

--- WifiScrren.js
+++ WifiScrren.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, PermissionsAndroid, StyleSheet } from 'react-native';
+import { View, Text, PermissionsAndroid, StyleSheet, ScrollView } from 'react-native';
 import wifi from 'react-native-android-wifi';
 import RNFetchBlob from 'react-native-fetch-blob';
 
 const WifiScreen = () => {
-
-    const [result, setResult] = useState('');
     const [connectedDeviceInfo, setConnectedDeviceInfo] = useState('');
+    const [availableConnection, setAvailableConnection] = useState([]);
 
 
     useEffect(() => {
         try {
+            // permission to access location to set wifi connection
             const granted = PermissionsAndroid.request(
                 PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
                 {
@@ -27,50 +27,72 @@ const WifiScreen = () => {
         } catch (err) {
             console.warn(err)
         }
+
+        // getting list of available wifi connection
+        wifi.loadWifiList((wifiStringList) => {
+            var wifiArray = JSON.parse(wifiStringList);
+            setAvailableConnection(wifiArray)
+        },
+            (error) => {
+                console.log(error);
+            }
+        );
+
+        // getting list of connected devices to phone hotspot
+        RNFetchBlob.fs.readStream("/proc/net/arp", 'utf8')
+            .then((stream) => {
+                let data = ''
+                stream.open()
+                stream.onData((chunk) => {
+                    data += chunk
+                })
+                stream.onEnd(() => {
+                    setConnectedDeviceInfo(data);
+                    console.log(data)
+                })
+            }).catch(err => {
+
+            });
     }, []);
 
-    // wifi.isEnabled((isEnabled) => {
-    //     if (isEnabled) {
-    //         console.log("wifi service enabled");
-    //     } else {
-    //         console.log("wifi service is disabled");
-    //     }
-    // });
-
-    // wifi.loadWifiList((wifiStringList) => {
-    //     var wifiArray = JSON.parse(wifiStringList);
-    //     console.log(wifiArray);
-    // },
-    //     (error) => {
-    //         console.log(error);
-    //     }
-    // );
-
-
-    RNFetchBlob.fs.readStream("/proc/net/arp", 'utf8')
-        .then((stream) => {
-            let data = ''
-            stream.open()
-            stream.onData((chunk) => {
-                data += chunk
-            })
-            stream.onEnd(() => {
-                setConnectedDeviceInfo(data);
-                console.log(data)
-            })
-        })
-
-
     return (
-        <View>
-            <Text style={{fontSize:20}}>List of connected device:</Text>
-            <Text style={{ fontSize: 12 }}>{connectedDeviceInfo}</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                <Text style={styles.textHeading}>{"List of connected device:\n"}</Text>
+                <Text style={{ fontSize: 12 }}>{connectedDeviceInfo}</Text>
+                <Text style={styles.textHeading}>{"\nAvailable wifi Connection:\n"}</Text>
+                {availableConnection.length > 0
+                    ? availableConnection.map((list, i) => {
+                        return (
+                            <View key={i}>
+                                <Text>{"BSSID:  " + list.BSSID}</Text>
+                                <Text>{"SSID:   " + list.SSID}</Text>
+                                <Text>{"capabilities:   " + list.capabilities}</Text>
+                                <Text>{"frequency:  " + list.frequency}</Text>
+                                <Text>{"level:  " + list.level}</Text>
+                                <Text>{"timestamp:  " + list.timestamp + "\n\n"}</Text>
+                            </View>
+                        )
+                    })
+
+                    : <Text>No connection available</Text>}
+            </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    text: { fontSize: 30, justifyContent: 'center', alignItems: 'center' }
+    container: {
+        margin: 10
+    },
+    text: {
+        fontSize: 30,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textHeading: {
+        fontSize: 20
+    }
 })
 
 export default WifiScreen;
