87874c85b8fd73a2dff8aa9282917366833e0fcd
Shilpa Shendre <shilpashendre@winjit.com>
2020-06-22 19:27:43 +0530

    updated code

--- screens/WifiScrren.js
+++ screens/WifiScrren.js
@@ -2,35 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, PermissionsAndroid, StyleSheet, ScrollView } from 'react-native';
 import wifi from 'react-native-android-wifi';
 import RNFetchBlob from 'react-native-fetch-blob';
+import NetInfo from "@react-native-community/netinfo";
+import Devices from 'react-native-device-info';
+import GetLocation from 'react-native-get-location';
 
 const WifiScreen = () => {
     const [connectedDeviceInfo, setConnectedDeviceInfo] = useState('');
     const [availableConnection, setAvailableConnection] = useState([]);
+    const [connectedTo, setConnectedTo] = useState("");
+    const [deviceMacAddress, setDeviceMacAddress] = useState("");
+    const [latlong, setLatlong] = useState([]);
 
 
     useEffect(() => {
         persmission();
         getConnectedDevices();
+        getConnectionInfo();
+        getDeviceDetails();
 
     }, []);
 
     const persmission = async () => {
         try {
             // permission to access location to set wifi connection
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-                {
-                    'title': 'Wifi networks',
-                    'message': 'We need your permission in order to find wifi networks'
-                }
-            ).then(res => {
-                if (res === "granted") {
-                    console.log("Thank you for your permission! :)");
-                    getWifiList();
-                } else {
-                    console.log("You will not able to retrieve wifi available networks list");
-                }
-            });
+            const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+                .then(res => {
+                    if (res === "granted") {
+                        console.log("Thank you for your permission! :)");
+                        getWifiList();
+                        getLatLong();
+                    } else {
+                        console.log("You will not able to retrieve wifi available networks list");
+                    }
+                });
         } catch (err) {
             console.warn(err)
         }
@@ -63,12 +67,86 @@ const WifiScreen = () => {
 
             });
     }
+
+    const getConnectionInfo = async () => {
+        NetInfo.fetch().then(connection => {
+
+            if (connection !== undefined) {
+                setConnectedTo(connection)
+            }
+
+        });
+    }
+
+
+    const getDeviceDetails = async () => {
+        Devices.getMacAddress().then(res => {
+            setDeviceMacAddress(res);
+        }).catch(err => {
+            console.log("TCL: WifiScreen -> err", err)
+
+        })
+        // Devices.getIpAddress().then(res => {
+        //     console.log("TCL: getIpAddress -> res", res)
+
+        // });
+    }
+
+    const getLatLong = async () => {
+        await GetLocation.getCurrentPosition({
+            enableHighAccuracy: true,
+            timeout: 15000,
+        })
+            .then(async location => {
+                setLatlong(location)
+            }).catch(error => {
+                const { code, message } = error;
+                console.warn(code, message);
+            });
+    }
+
+
     return (
         <ScrollView>
             <View style={styles.container}>
-                <Text style={styles.textHeading}>{"List of connected device:\n"}</Text>
-                <Text style={{ fontSize: 12 }}>{connectedDeviceInfo}</Text>
-                <Text style={styles.textHeading}>{"\nAvailable wifi Connection:\n"}</Text>
+                <Text style={styles.textHeading}>Device Info:</Text>
+                <Text>{"HW address:  " + deviceMacAddress}</Text>
+                <Text>{"latitude:  " + latlong.latitude}</Text>
+                <Text>{"latitude:  " + latlong.latitude}</Text>
+                <Text>{"timestamp:  " + latlong.time + "\n"}</Text>
+
+                <Text style={styles.textHeading}>Connected network details :</Text>
+                {connectedTo !== "" && connectedTo.type === 'wifi'
+                    ? <View>
+                        <Text>{"connected type:   " + connectedTo.type}</Text>
+                        <Text>{"isConnected:   " + connectedTo.isConnected}</Text>
+                        <Text>{"isInternetReachable:   " + connectedTo.isInternetReachable}</Text>
+                        <Text>{"isWifiEnabled:   " + connectedTo.isWifiEnabled}</Text>
+
+                        <Text>{"bssid:   " + connectedTo.details.bssid}</Text>
+                        <Text>{"frequency:   " + connectedTo.details.frequency}</Text>
+                        <Text>{"ipAddress:   " + connectedTo.details.ipAddress}</Text>
+                        <Text>{"isConnectionExpensive:   " + connectedTo.details.isConnectionExpensive}</Text>
+                        <Text>{"ssid:   " + connectedTo.details.ssid}</Text>
+                        <Text>{"strength:   " + connectedTo.details.strength}</Text>
+                        <Text>{"subnet:   " + connectedTo.details.bssid + "\n"}</Text>
+                    </View>
+
+                    : connectedTo !== "" && connectedTo.type === 'cellular'
+                        ? <View>
+                            <Text>{"connected to:   " + connectedTo.type}</Text>
+
+                            <Text>{"carrier:   " + connectedTo.details.carrier}</Text>
+                            <Text>{"cellularGeneration:   " + connectedTo.details.cellularGeneration}</Text>
+                            <Text>{"isConnectionExpensive:   " + connectedTo.details.isConnectionExpensive + "\n"}</Text>
+
+
+                            <Text style={styles.textHeading}>{"List of device connected to mobile hotspot:\n"}</Text>
+                            <Text style={{ fontSize: 12 }}>{connectedDeviceInfo}</Text>
+                        </View>
+                        : <Text>no connection found</Text>}
+
+                <Text style={styles.textHeading}>{"Available wifi Connection:\n"}</Text>
                 {availableConnection.length > 0
                     ? availableConnection.map((list, i) => {
                         return (
@@ -78,14 +156,17 @@ const WifiScreen = () => {
                                 <Text>{"capabilities:   " + list.capabilities}</Text>
                                 <Text>{"frequency:  " + list.frequency}</Text>
                                 <Text>{"level:  " + list.level}</Text>
-                                <Text>{"timestamp:  " + list.timestamp + "\n\n"}</Text>
+                                <Text>{"timestamp:  " + list.timestamp + "\n"}</Text>
                             </View>
                         )
                     })
 
                     : <Text>No connection available</Text>}
+
+
+
             </View>
-        </ScrollView>
+        </ScrollView >
     )
 }
 
@@ -99,7 +180,8 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     textHeading: {
-        fontSize: 20
+        fontSize: 20,
+        color: 'red'
     }
 })
 
