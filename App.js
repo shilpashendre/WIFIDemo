9bd8c847962e0d93ac9d827c541d3e7ae79dd445
Shilpa Shendre <shilpashendre@winjit.com>
2020-06-26 14:53:12 +0530

    simple app for lib

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, PermissionsAndroid } from 'react-native';
+import LibraryFunction from './screens/LibraryFunction';
+import NetInfoeDevice from './NetPackage/NetLib';
 
 const App = () => {
+
+
+    NetInfoeDevice.fetch().then(connection => {
+        console.log("TCL: App -> connection", connection)
+        if (connection !== undefined) {
+            // setConnectedTo(connection)
+        }
+    });
+
     return (
         <View>
             <Text>abcd</Text>
--- index.js
+++ index.js
@@ -5,10 +5,9 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-import LibraryFunction from './screens/LibraryFunction';
 import NetInfoeDevice from './NetPackage/NetLib';
 
 
-export { LibraryFunction, NetInfoeDevice };
+export { NetInfoeDevice };
 
 AppRegistry.registerComponent(appName, () => App);
